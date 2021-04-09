20d10555fcc810514a2efdc4fa82b9523b011159
Vanessa Su <vnessa.su@gmail.com>
2021-04-09 13:24:34 -0400

    Add Pizza.prototype.calculatePrice method that passes no toppings test case

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 // ---------- Business Logic ----------
 // *** Pizza ****
-function Pizza(toppingsSelected, sizeSelected) {
+function Pizza(toppingsSelected, sizeSelected){
   this.toppings = toppingsSelected;
   this.size = sizeSelected;
+}
+
+Pizza.prototype.calculatePrice = function(){
+  const size = this.size;
+  let price;
+  switch (size){
+    case ("small"):
+      price = 5.00;
+      break;
+    case ("medium"):
+      price = 8.00;
+      break;
+    case ("large"):
+      price = 10.00;
+      break;
+    default:
+      throw "Invalid pizza size";
+  }
+  this.price = price;
 }
 
 // ---------- User Interface Logic ----------
@@ -20,6 +39,7 @@ $(document).ready(function(){
     const pizzaSize = $("#pizzaSize").val();
     const pizzaToppings = getPizzaToppingsSelected();
     const inputPizza = new Pizza(pizzaToppings, pizzaSize);
+    inputPizza.calculatePrice();
     console.log(pizzaSize);
     console.log(pizzaToppings);
     console.log(inputPizza);

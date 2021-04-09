d61f094ad356e2d225519eeaca577c183251d1ff
Vanessa Su <vnessa.su@gmail.com>
2021-04-09 14:04:06 -0400

    Add UI function to display pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,13 @@ Pizza.prototype.calculatePrice = function(){
   const numberOfToppings = this.toppings.length;
   let price;
   switch (size){
-    case ("small"):
+    case ("Small"):
       price = 5.00;
       break;
-    case ("medium"):
+    case ("Medium"):
       price = 8.00;
       break;
-    case ("large"):
+    case ("Large"):
       price = 10.00;
       break;
     default:
@@ -28,10 +28,11 @@ Pizza.prototype.calculatePrice = function(){
 
 // TEST !!!!!TO BE REMOVED!!!!!
 let toppings = ["cheese", "pepperoni", "green peppers"];
-let size = "medium";
+let size = "Medium";
 const customerPizza = new Pizza(toppings, size);
 customerPizza.calculatePrice();
 customerPizza;
+// displayPizzaPrice(customerPizza);
 
 // ---------- User Interface Logic ----------
 function getPizzaToppingsSelected(){
@@ -42,6 +43,28 @@ function getPizzaToppingsSelected(){
   return toppingsSelected;
 }
 
+function displayPizzaPrice(pizza){
+  const pizzaSize = pizza.size;
+  const pizzaToppings = pizza.toppings;
+  const numberOfToppings = pizzaToppings.length;
+  const pizzaPrice = pizza.price;
+  console.log(pizzaSize);
+  console.log(pizzaToppings);
+  console.log(pizzaPrice);
+  console.log(pizzaToppings.length)
+  let htmlString = `<p class="pizza-size">${pizzaSize} Pizza</p><p class="pizza-toppings">`;
+  pizzaToppings.forEach(function(element, index){
+    htmlString += element;
+    if(index === numberOfToppings-1){
+      htmlString += "</p>";
+    } else {
+      htmlString += ", ";
+    }
+  });
+  htmlString += `<p class="pizza-price">$${pizzaPrice.toFixed(2)}</p>`;
+  $("#priceDisplay").html(htmlString);
+}
+
 $(document).ready(function(){
   $("#pizzaForm").submit(function(event){
     event.preventDefault();
@@ -49,6 +72,7 @@ $(document).ready(function(){
     const pizzaToppings = getPizzaToppingsSelected();
     const inputPizza = new Pizza(pizzaToppings, pizzaSize);
     inputPizza.calculatePrice();
+    displayPizzaPrice(inputPizza);
     console.log(pizzaSize);
     console.log(pizzaToppings);
     console.log(inputPizza);
